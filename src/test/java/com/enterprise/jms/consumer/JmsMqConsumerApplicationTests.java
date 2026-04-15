b0d488271be7c8de23f83a822bb9008790f28163
Praveen Kumar <pravi.sma1954@gmail.com>
2026-04-15 23:57:19 +0530

    Restore JMS consumer application sources

---
+++ src/test/java/com/enterprise/jms/consumer/JmsMqConsumerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.enterprise.jms.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsMqConsumerApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
